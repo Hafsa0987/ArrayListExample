9073d3ed42dfcde15211dbf45c52acfac1977a0d
Hafsa0987 <hafsasaghir06@gmail.com>
2025-03-17 21:37:16 +0500

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.arraylist;
+import java.util.ArrayList;
+/**i
+ *
+ * @author SR Laptop
+ */
+public class Employee {
+      String name;
+int salary;
+    Employee(String name, int salary){
+    this.name=name;
+    this.salary=salary;
+    
+    
+}
+public void DisplayInfo(){
+    System.out.println("Name:" +name+ "\nSalary:"+salary);
+}
+        
+}
+
